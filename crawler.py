7c0bd3ef545b6fa7313ac29533966156fc150785
Unknown <vincentfugnitto94@gmail.com>
2017-11-14 18:36:18 -0500

    Updated crawler to extract relevant page data

--- crawler.py
+++ crawler.py
@@ -2,7 +2,9 @@
 from bs4 import BeautifulSoup
 import json
 import re
-import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 
 # page bound
 bound = 10
@@ -20,18 +22,55 @@ parent_urls = [
 
 
 def get_urls():
-    ''' test function to extract html links '''
+    ''' extract all possible url '''
+    print('=== Started: Extracting list of possible URLs... ===')
+    # open and write to file
     f = open('content/urls.txt', 'w')
+    # iterate through each url within parent_urls list
+    http = urllib3.PoolManager()
     for url in parent_urls:
-        request = requests.get(url)
-        soup = BeautifulSoup(request.text, 'html.parser')
+        request = http.request('GET', url)
+        soup = BeautifulSoup(request.data.decode('utf-8'), 'html.parser')
+        # retrieve all url beginning with http:// or https://
         for link in soup.find_all('a', href=True):
             if str(link['href']).startswith(('http://', 'https://')):
+                # write url to file
                 f.write(str(link['href']) + "\n")
+    f.close()
+    print('=== Finished: URLs written to content/urls.txt ===')
+
+
+def get_content(url):
+    ''' obtain webpage content (title, h1-h6 tags, p, span tags) '''
+    # store data within json
+    data = {}
+    content = list()
+    # use urrlib3 and beautifulsoup to requet and parse html pages
+    http = urllib3.PoolManager()
+    request = http.request('GET', url)
+    soup = BeautifulSoup(request.data.decode('utf-8'), 'html.parser')
+    # parse webpage for content ===
+    # obtain page title
+    title = soup.title.text
+    # obtain page headers (h1 to h6)
+    for i in range(1,6):
+        header = [tag.text for tag in soup.findAll('h%s' % i)]
+        if header: content.append(header)
+    # add p tags
+    paragraph = [tag.text for tag in soup.findAll('p')]
+    content.append(paragraph)
+    # add span tags
+    span = [tag.text for tag in soup.findAll('span')]
+    content.append(paragraph)
+    # store content into dict
+    data['title'] = title
+    data['content'] = content
+    print(str(data))
 
 
 def main():
-    get_urls()
+    # get_urls()
+    get_content(parent_urls[0])
 
 
 if __name__ == '__main__':
