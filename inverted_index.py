e7dbab58f2afa197ed75e2953685206a9c7963de
Unknown <vincentfugnitto94@gmail.com>
2017-11-25 21:07:50 -0500

    Added argparse for inverted_index.py build settings

--- inverted_index.py
+++ inverted_index.py
@@ -1,15 +1,18 @@
-from afinn import Afinn
-import json
+import argparse
 import collections
+import json
+
+from afinn import Afinn
+
 
 # Creates a "inverted_index.txt" file containing the inverted index
 def create_inverted_index():
     with open('result.json') as json_data:
-        inverted_index = dict()         # Dictionary representing inverted index
-        data = json.load(json_data)     # Data retrieved from json file created by crawler
+        inverted_index = dict()  # Dictionary representing inverted index
+        data = json.load(json_data)  # Data retrieved from json file created by crawler
 
         for entry in data:
-            id = entry["url"]       # Gets URL value from article
+            id = entry["url"]  # Gets URL value from article
             tokens = entry["text"]  # Gets a list of tokens from text
 
             # Adds tokens to dictionary while keeping track of frequency
@@ -36,8 +39,8 @@ def create_inverted_index():
 # Retrieves inverted index as a dictionary
 def load_inverted_index():
     with open("inverted_index.txt", "r") as f:
-        f.readline()    # Discards empty line
-        index = {}      # Dictionary representing inverted index
+        f.readline()  # Discards empty line
+        index = {}  # Dictionary representing inverted index
 
         # Adds each line from file to dictionary
         for line in f.readlines():
@@ -54,14 +57,12 @@ def load_inverted_index():
 # Scores content based on the afinn dictionary
 # Set type to s for strings, l for lists
 def sentiment_score(content, type='s'):
-    afinn = Afinn()     # Afinn dictionary library object
-    score = 0.0         # Sentiment score value
-
+    afinn = Afinn()  # Afinn dictionary library object
+    score = 0.0  # Sentiment score value
     if type == 's':
         score = afinn.score(content)
     elif type == 'l':
         score = afinn.score(' '.join(content))
-
     return score
 
 
@@ -69,15 +70,15 @@ def sentiment_score(content, type='s'):
 def create_doc_stats():
     with open("doc_stats.txt", "w") as f:
         with open("result.json", "r") as json_data:
-            data = json.load(json_data)                 # Retrieves data from json file created by crawler
-            doc_stats = collections.OrderedDict()       # Dictionary holding length and sentiment values for all URLs
+            data = json.load(json_data)  # Retrieves data from json file created by crawler
+            doc_stats = collections.OrderedDict()  # Dictionary holding length and sentiment values for all URLs
 
             for entry in data:
-                id = entry["url"]                                   # Gets URL
-                tokens = entry["text"]                              # Gets a list of tokens from text
-                score = sentiment_score(tokens, 'l')                # Scores all tokens
-                doc_stats[id] = (len(tokens), score)                # Sets length and sentiment as stats
-                f.write("%s %s %s\n" % (id, len(tokens), score))    # Write URL, length and sentiment values
+                id = entry["url"]  # Gets URL
+                tokens = entry["text"]  # Gets a list of tokens from text
+                score = sentiment_score(tokens, 'l')  # Scores all tokens
+                doc_stats[id] = (len(tokens), score)  # Sets length and sentiment as stats
+                f.write("%s %s %s\n" % (id, len(tokens), score))  # Write URL, length and sentiment values
 
 
 # Returns a dictionary containing all crawled URL with their length and sentiment value
@@ -86,8 +87,8 @@ def load_doc_stats():
 
     with open("doc_stats.txt", "r") as f:
         for line in f.readlines():
-            parts = line.split(' ')                                     # Split line into list
-            doc_stats[parts[0]] = (int(parts[1]), float(parts[2]))      # Set length and sentiment values to each URL
+            parts = line.split(' ')  # Split line into list
+            doc_stats[parts[0]] = (int(parts[1]), float(parts[2]))  # Set length and sentiment values to each URL
 
     return doc_stats
 
@@ -95,10 +96,10 @@ def load_doc_stats():
 # Returns a list of matching URLs that contain the keyword(s) in the search string.
 # It is assumed that the keywords have an implicit AND between them.
 def search(search_string, inverted_index):
-    search_terms = search_string.split()                    # Gets list of query terms
-    search_score = sentiment_score(search_string, 's')      # Gets sentiment score for all query terms
-    is_positive = search_score >= 0                         # Gets whether query is positive or negative
-    doc_stats = load_doc_stats()                            # Loads length and sentiment values for all docs
+    search_terms = search_string.split()  # Gets list of query terms
+    search_score = sentiment_score(search_string, 's')  # Gets sentiment score for all query terms
+    is_positive = search_score >= 0  # Gets whether query is positive or negative
+    doc_stats = load_doc_stats()  # Loads length and sentiment values for all docs
 
     postings_list = list()
 
@@ -120,7 +121,6 @@ def search(search_string, inverted_index):
 
     # Sorts based on the sentiment score of the URLs
     sorted_results = sorted(results, key=lambda x: doc_stats[x][1], reverse=is_positive)
-
     return sorted_results
 
 
@@ -130,18 +130,27 @@ class TermDict(dict):
         self.sentiment = 0
 
 
-# create_inverted_index()
-index = load_inverted_index()
-# create_doc_stats()
-doc_stats = load_doc_stats()
-#print(index)
+def run():
+    """ run inverted index """
+    parser = argparse.ArgumentParser(description='define build settings')
+    parser.add_argument('-build', action='store_true')
+    args = parser.parse_args()
+    if args.build:
+        print("building inverted index...")
+        create_inverted_index()
+        create_doc_stats()
+        print("inverted index complete.")
+    index = load_inverted_index()
+    doc_stats = load_doc_stats()
+    while True:
+        query = input("\nplease provide a query:\n")
+        if query is not None:
+            results = search(str(query), index)
+            for result in results:
+                print(result, doc_stats[result][1])
+        else:
+            continue
 
 
-while True:
-    query = input("\nplease provide a query:\n")
-    if query is not None:
-        results = search(str(query), index)
-        for result in results:
-            print(result, doc_stats[result][1])
-    else:
-        continue
+if __name__ == '__main__':
+    run()
